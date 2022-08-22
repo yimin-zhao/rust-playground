4fb5b83ffa1b86dc968cdaa03ec342b6b2f07f77
Evan Zhao <tacingiht@gmail.com>
2022-08-22 17:30:09 +0800

    Add error_handling (#1)
    Signed-off-by: Yimin Zhao <tacinght@gmail.com>
    
    Signed-off-by: Yimin Zhao <tacinght@gmail.com>
    Co-authored-by: Yimin Zhao <tacinght@gmail.com>

---
+++ error_handling/src/main.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(rc) => rc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        }
+    };
+
+    println!("Open file: {:?}", f);
+
+    let f2 = File::open("wonder.txt").expect("Failed to open wonder world");
+    let f1 = File::open("world.txt").unwrap();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
